61395afc75d8d10dd210372c32a6e469ddc5afb1
Jacques Schmitz Junior <juniorschmitz9@gmail.com>
2023-08-02 17:27:33 -0300

    refactor: changing config to use headless on CI

--- playwright.config.js
+++ playwright.config.js
@@ -31,7 +31,7 @@ module.exports = defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     // Headless browser
-    headless: false
+    headless: true
   },
   
 
