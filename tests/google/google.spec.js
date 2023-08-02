748f3777bcf3b70bd821d39bcb5f6095a910f65d
Jacques Schmitz Junior <juniorschmitz9@gmail.com>
2023-08-02 17:23:13 -0300

    add: clean start project with some examples

---
+++ tests/google/google.spec.js
@@ -0,0 +1,11 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test.describe('Google', () => {
+  test('Searching for Playwright shows title', async ({ page }) => {
+    await page.goto('https://google.com/');
+    await page.fill('textarea[name="q"]', 'playwright');
+    await page.press('textarea[name="q"]', 'Enter');
+    await expect(page).toHaveTitle(/playwright/);
+  });
+});
---
+++ tests/parabank/login.spec.js
@@ -0,0 +1,16 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test.describe('Parabank', () => {
+  test('Login', async ({ page }) => {
+    await page.goto('https://parabank.parasoft.com/parabank/index.htm');
+    await page.fill('input[name="username"]', 'john');
+    await page.fill('input[name="password"]', 'demo');
+    await page.click('input[value="Log In"]');
+
+    const pLoggedIn = await page.locator('p.smallText');
+    const messageLoggedIn = await pLoggedIn.innerText();
+    expect(messageLoggedIn).toContain('John Smith');
+    await page.screenshot({ path: 'screenshot.png', fullPage: true });
+  });
+});
