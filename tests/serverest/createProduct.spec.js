7b15d8a8453388b9faf8489fc5043a9895900f47
Jacques Schmitz Junior <juniorschmitz9@gmail.com>
2023-08-04 19:57:56 -0300

    fiox

--- tests/serverest/createProduct.spec.js
+++ tests/serverest/createProduct.spec.js
@@ -16,7 +16,7 @@ test.describe('Serverest', () => {
     await homePage.validateLoggedIn();
   });
 
-  test.only('Create Product @ci', async ({ page }) => {
+  test('Create Product @ci', async ({ page }) => {
     await homePage.accessProductPage();
 
     let productName = faker.internet.userName() + "123";
